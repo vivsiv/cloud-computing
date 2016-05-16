f34543c92fb4c0517d6e3e75a548966262041109
hc934 <wtchen@cs.ucla.edu>
2016-05-16 02:31:28 -0700

    link prediction using features vector

--- user-restaurant-data-proc.py
+++ user-restaurant-data-proc.py
@@ -24,28 +24,30 @@ lv_clustering_data = lv_restaurants.map(lambda r:
 		[r.business_id,"None" if len(r.neighborhoods) is 0 else r.neighborhoods[0],r.stars,r.attributes['Price Range']]
 	).toDF(['business_id','neighborhood','stars','price_range'])
 
+# drop row with null values
+lv_clustering_data = lv_clustering_data.dropna()
+
 #Neighborhood feature engineering
-# stringIndexer = StringIndexer(inputCol="neighborhood", outputCol="neigh_index")
-# lv_model = stringIndexer.fit(lv_clustering_data)
-# lv_indexed = lv_model.transform(lv_clustering_data)
-# encoder = OneHotEncoder(dropLast=False, inputCol="neigh_index", outputCol="neigh_vec")
-# lv_encoded = encoder.transform(lv_indexed)
+stringIndexer = StringIndexer(inputCol="neighborhood", outputCol="neigh_index")
+lv_model = stringIndexer.fit(lv_clustering_data)
+lv_indexed = lv_model.transform(lv_clustering_data)
+encoder = OneHotEncoder(dropLast=False, inputCol="neigh_index", outputCol="neigh_vec")
+lv_encoded = encoder.transform(lv_indexed)
 
-# assembler = VectorAssembler(
-#     inputCols=["stars", "price_range", "neigh_vec"],
-#     outputCol="features_vec")
-# lv_assembled = assembler.transform(lv_encoded)
+assembler = VectorAssembler(
+    inputCols=["stars", "price_range", "neigh_vec"],
+    outputCol="features_vec")
+lv_assembled = assembler.transform(lv_encoded)
 
 
-
-neighborhoods = lv_clustering_data.groupBy("neighborhood").count().select("neighborhood").collect()
-for n in neighborhoods:
-	statement = "SELECT *, neighborhood='{0}' AS {0} FROM __THIS__".format(n['neighborhood'].replace(" ","_"))
-	sqlTrans = SQLTransformer(statement=statement)
-	lv_clustering_data = sqlTrans.transform(lv_clustering_data)
-
-col_drop_list = ['business_id', 'neighborhood']
-lv_clustering = lv_clustering_data.select([column for column in lv_clustering_data.columns if column not in col_drop_list])
+# neighborhoods = lv_clustering_data.groupBy("neighborhood").count().select("neighborhood").collect()
+# for n in neighborhoods:
+# 	statement = "SELECT *, neighborhood='{0}' AS {0} FROM __THIS__".format(n['neighborhood'].replace(" ","_"))
+# 	sqlTrans = SQLTransformer(statement=statement)
+# 	lv_clustering_data = sqlTrans.transform(lv_clustering_data)
+#
+# col_drop_list = ['business_id', 'neighborhood']
+# lv_clustering = lv_clustering_data.select([column for column in lv_clustering_data.columns if column not in col_drop_list])
 
 
 #generate lv_restaurant vertices set
@@ -70,55 +72,45 @@ edges = lv_reviews.withColumnRenamed("user_id","src").withColumnRenamed("busines
 
 #### Cluster ####
 # create cluter for testing use
-num_of_clusters = 10
-test = lv_clustering_data.select("stars","price_range")
-clusters = KMeans.train(test.dropna().map(list), num_of_clusters, maxIterations=10, runs=10,initializationMode="random")
+num_of_clusters = 50
+features_vec = lv_assembled.select("features_vec").map(lambda r: r.features_vec).collect()
+clusters = KMeans.train(sc.parallelize(features_vec), num_of_clusters, maxIterations=10, runs=10,initializationMode="random")
 
 # find highest rating restaurants for each user
 g = GraphFrame(vertices, edges)
-gmax = g.edges.select("src","dst","stars").groupBy("src").max("stars").withColumnRenamed("max(stars)","max_stars")
-gjoin = g.edges.join(gmax,g.edges.src==gmax.src).select(g.edges.src,g.edges.dst,g.edges.stars,gmax.max_stars).filter("stars=max_stars").dropDuplicates(['src'])
+g_max = g.edges.select("src","dst","stars").groupBy("src").max("stars").withColumnRenamed("max(stars)","max_stars")
+g_max_edge = g.edges.join(g_max,g.edges.src==g_max.src).select(g.edges.src,g.edges.dst,g.edges.stars,g_max.max_stars).filter("stars=max_stars").dropDuplicates(['src'])
 
 # add restaurant feature vectors to edge
-gjoin_restaurant_info = gjoin.join(lv_clustering_data,gjoin.dst==lv_clustering_data.business_id).select(gjoin.src,gjoin.dst,gjoin.max_stars,lv_clustering_data.neighborhood,lv_clustering_data.price_range,lv_clustering_data.stars)
-def addCluster(stars,price_range): return clusters.predict(array([stars,price_range]))
-clusterAdder = udf(addCluster,IntegerType())
-g_all = gjoin_restaurant_info.withColumn("cluster_id", clusterAdder(gjoin_restaurant_info['stars'],gjoin_restaurant_info['price_range']))
-lv_all = lv_clustering_data.withColumn("cluster_id", clusterAdder(lv_clustering_data['stars'],lv_clustering_data['price_range']))
+g_features = g_max_edge.join(lv_assembled,g_max_edge.dst==lv_assembled.business_id).select(g_max_edge.src,g_max_edge.dst,lv_assembled.features_vec)
 
-###### runnable ########
+# append cluster_id to each restaurants in the edge
+def addCluster(features_vec): return clusters.predict(features_vec)
+clusterAdder = udf(addCluster,IntegerType())
+g_all = g_features.withColumn("cluster_id", clusterAdder(g_features['features_vec']))
+lv_all = lv_assembled.select("business_id","features_vec").withColumn("cluster_id", clusterAdder(g_features['features_vec']))
+
 # construct cluster index
 cluster_index = {}
 for i in range(num_of_clusters):
 	cluster_index[i] = lv_all.filter(lv_all.cluster_id == i).select("business_id").map(lambda r: r.business_id).collect()
+	lv_all = lv_all.filter(lv_all.cluster_id != i)
+
+# list of all edges
+all_edges = g.edges.select("src","dst").map(lambda r: array([r.src,r.dst])).collect()
 
 def addPrediction(cluster_id): return random.choice(cluster_index[cluster_id])
+# def addPrediction(user_id,cluster_id):
+	# predict_dst = random.choice(cluster_index[cluster_id])
+	# while filter(lambda x: x[0]==user_id and x[1]==predict_dst,all_edges) != []:
+	# 	predict_dst = random.choice(cluster_index[cluster_id])
+	# return predict_dst
 predictionAdder = udf(addPrediction, StringType())
+# g_all = g_all.withColumn("predict_dst", predictionAdder(g_all['src'],g_all['cluster_id']))
 g_all = g_all.withColumn("predict_dst", predictionAdder(g_all['cluster_id']))
 
+# newly predicted edges
 new_edges = g_all.select("src","predict_dst").withColumnRenamed("predict_dst","dst")
 
 # todo
 # filter out the restaurants the user has already visited
-
-# get list of all restaurants
-# candidates = lv_reviews.select("business_id").dropDuplicates().map(lambda r: r.business_id).collect() #4658
-#
-# # add a candidate column to gjoin_restaurant_info
-# def addCandidate(arg): return candidates
-# candidateAdder = udf(addCandidate,ArrayType(StringType()))
-# g_edge_all = gjoin_restaurant_info.withColumn("candidates", candidateAdder(gjoin_restaurant_info['src']))
-#
-# # find restaurants in the same clusters
-# def findSameCluster(row):
-#     fv1 = array([row.stars,row.price_range])
-#     def f(c):
-#         r = lv_clustering_data.filter(lv_clustering_data.business_id==c).select("stars","price_range").first()
-#         fv2 = array([r.stars,r.price_range])
-#         return clusters.predict(fv1) == clusters.predict(fv2)
-#     filter(f,row.candidates)
-#
-# g_edge_all.foreach(findSameCluster)
-
-
-# 2. add the edges for these user-restaurant pairs
