cbad8d05550566e541a9bb3622103321950ff775
Vivek <vivek.a.sivakumar@gmail.com>
2016-05-27 19:01:54 -0700

    add most influential link predictions

--- friend-graph-data-proc.py
+++ friend-graph-data-proc.py
@@ -1,40 +1,112 @@
 #TO RUN open shell with command:pyspark --packages graphframes:graphframes:0.1.0-spark1.6
 from pyspark.sql import SQLContext
 from graphframes import *
+import random
+from pyspark.sql.functions import udf
+from pyspark.sql.functions import lit
+from pyspark.sql.types import *
+from pyspark.sql.functions import col
 
 sqlContext = SQLContext(sc)
 
-#load user data
+#load data
 users = sqlContext.read.json("data/yelp_academic_dataset_user.json")
 #552339
-
-#load business data
 business = sqlContext.read.json("data/yelp_academic_dataset_business.json")
-#get just Vegas businesses
-lv_business = business.filter(business["city"] == "Las Vegas")
-#get just vegas restaurants
-lv_restaurants = lv_business.select("*").where("array_contains(categories,'Restaurants')")
-#load review data
 review = sqlContext.read.json("data/yelp_academic_dataset_review.json")
+
+#get just Vegas restaurants
+lv_business = business.filter(business["city"] == "Las Vegas")
+lv_restaurants = lv_business.select("*").where("array_contains(categories,'Restaurants')")
+
 #get user ids who have reviewed lv_businesses
 lv_user_ids = review.join(lv_restaurants.select("business_id"),"business_id").groupBy("user_id").count().select("user_id")
+
+lv_reviews = review.join(lv_restaurants.select("business_id"),"business_id").select("business_id","user_id","stars","date")
+
+
+
+### FRIEND GRAPH ###
 #compute vertices
-vertices = lv_user_ids.withColumnRenamed("user_id","id")
-#vertices.count()
+friend_vertices = lv_user_ids.withColumnRenamed("user_id","id")
+#friend_vertices.count()
 #174479
 
-#compute edges
+#compute friend_edges
 user_friends_pairs = users.map(lambda user: (user.user_id,user.friends))
 user_friend_edges = user_friends_pairs.flatMapValues(lambda user: user).toDF(['user_id','friend_id'])
-edges = user_friend_edges.withColumnRenamed("user_id","src").withColumnRenamed("friend_id","dst")
+friend_edges = user_friend_edges.withColumnRenamed("user_id","src").withColumnRenamed("friend_id","dst")
 
-friend_graph = GraphFrame(vertices, edges)
+friend_graph = GraphFrame(friend_vertices, friend_edges)
 
-# print friend_graph
 
-# friend_graph.vertices.show()
-# friend_graph.edges.show()
+### BUSINESS GRAPH ###
+#generate lv_restaurant vertices set
+lv_vertices = lv_restaurants.select("business_id").withColumnRenamed("business_id","id").withColumn("type",lit("business"))
+#generate business graph user vertices
+users_vertices = users.select("user_id").withColumnRenamed("user_id","id").withColumn("type",lit("user"))
+business_vertices = users_vertices.unionAll(lv_vertices)
 
-cc = friend_graph.connectedComponents()
+#load review data
+# lv_reviews = review.join(lv_restaurants.select("business_id"),"business_id").select("business_id","user_id","stars","date")
 
-#cc = friend_graph.connectedComponents().vertices
+#user-business graph edges
+business_edges = lv_reviews.withColumnRenamed("user_id","src").withColumnRenamed("business_id","dst")
+business_graph = GraphFrame(business_vertices, business_edges)
+
+
+user_friends_pairs_df = users.map(lambda user: (user.user_id,user.friends)).toDF(['user_id','friends'])
+
+user_random_friend_df = user_friends_pairs_df.map(lambda user: (user.user_id, None if len(user.friends) == 0 else random.choice(user.friends))).toDF(['user_id','friend_id'])
+user_random_friend_df = user_random_friend_df.filter(user_random_friend_df['friend_id'] != 'null')
+
+def randomBusiness(friend_id):
+	business_ids = lv_reviews.filter(lv_reviews['user_id'] == friend_id).select('business_id')
+	return random.choice(business_ids.map(lambda b: b.business_id).collect())
+
+randomBusinessUDF = udf(randomBusiness, StringType())
+
+predictions_df = user_random_friend_df.map(randomBusinessUDF(user_random_friend_df['friend_id']))
+
+### SOCIAL NETWORK LINK PREDICTION ###
+# result = friend_graph.stronglyConnectedComponents(maxIter=10)
+
+# component_index = {}
+# for i in range(result.groupBy("component").max("component")):
+# 	cluster_index[i] = lv_all.filter(lv_all.cluster_id == i).select("business_id").map(lambda r: r.business_id).collect()
+# 	lv_all = lv_all.filter(lv_all.cluster_id != i)
+
+
+predictions = {}
+users_to_predict = user_friends_pairs_df.limit(10).collect()
+for user in users_to_predict:
+	restaurants = []
+	for friend in user.friends:
+		business_ids = lv_reviews.filter(lv_reviews['user_id'] == friend).select('business_id')
+		restaurants.append(business_ids.map(lambda b: b.business_id).collect())
+	if len(restaurants) > 0:
+		predictions[user.user_id] = random.choice(restaurants)
+
+def addPrediction(user_id): return prediction[user_id]
+predictionAdder = udf(addPrediction, StringType())
+
+business_graph = business_graph.withColumn("predict_dst",predictionAdder(business_graph['user_id']))
+new_edges = business_graph.select("src","predict_dst").withColumnRenamed("predict_dst","dst")
+	#get users friends to data frame
+	#get links for each user to restaurant in business graph
+	#choose a restaurant from set and predict link
+
+
+inDeg = friend_graph.inDegrees
+mostPopular = inDeg.sort(col("inDegree").desc()).limit(100)
+mostPop_rest = lv_reviews.join(b.select("id"), mostPopular.id == lv_reviews.user_id).select("business_id","user_id","stars","date")
+trainNum = int(round(mostPop_rest.count() * 0.5))
+mostPop_first_half = sqlContext.createDataFrame(mostPop_rest.orderBy("date").collect()[:trainNum])
+
+most_pop_rest_arr = mostPop_first_half.select("business_id").map(lambda r: r.business_id).collect()
+def addPrediction2(blah): return random.choice(most_pop_rest_arr)
+predictionAdder2 = udf(addPrediction2, StringType())
+business_graph_new = business_graph.edges.withColumn("predict_dst", predictionAdder2(business_graph.edges['src']))
+business_graph_predictions = business_graph_new.select("src","predict_dst").withColumnRenamed("predict_dst","dst")
+	
+
